501b0d4cc96b2b9b4de98b58fc333d944ecd8999
Shawn Guo <guoxian2@myvuw.ac.nz>
2025-08-12 11:27:06 +1200

    rebuild exploit

--- BuildDocker/convert_data_toString.sh
+++
@@ -1,9 +0,0 @@
-# copy the python script and json into docker
-chmod +x ort_output.sh
-./ort_output.sh
-docker cp ./ortProfExploit.py ort_test:/models/ortProfExploit.py
-docker cp ./result_ort.json ort_test:/models/result_ort.json
-
-# run python script
-docker exec -it ort_test bash -c "python3 /models/ortProfExploit.py"
-
